850881a0b5f65a4a327e2ae5d2b0f7e8c5b80ade
L Caraco <lbc.dna@gmail.com>
2021-02-27 13:39:37 -0600

    made tableau

--- sketch.js
+++ sketch.js
@@ -1,9 +1,34 @@
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    background(240);
+
+    new Tableau(0, 0, [4, 2, 1]).draw()
+    new Tableau(6, 0, [4, 2, 2, 1]).draw()
+    new Tableau(12, 0, [2, 2]).draw()
 }
 
 function draw() {
-    
+    try {
+        const focused = Renderer.renderAll().found;
+
+        // if (clickThisFrame) {
+        //     if (this.lastFocused && this.lastFocused != focused) {
+        //         this.lastFocused.loseFocus && this.lastFocused.loseFocus();
+        //     }
+
+        //     this.lastFocused = focused;
+
+        //     if (focused) {
+        //         focused.gainFocus && focused.gainFocus();
+        //     } else {
+        //         SceneManager.tray.loadMachineOptions();
+        //     }
+        // }
+    } catch (e) {
+        console.error(e);
+    } finally {
+        Renderer.clearStack();
+    }
 }
 
 function windowResized() { 
---
+++ tableau.js
@@ -0,0 +1,51 @@
+class Tableau {
+    static colors = ["#C9A5F3", "#F87060", "#FFB400", "#42BFDD", "#A6FFA1"]
+    static gridUnit;
+    static gridPosition = [200, 500];
+    static textSize = 30;
+
+    constructor(gridX, gridY, shape) {
+        Tableau.gridUnit = Renderer.textWidth("0", Tableau.textSize) * 1.15;
+        this.position = [gridX * Tableau.gridUnit, gridY * Tableau.gridUnit];
+        this.shape = shape;
+        this.color = random(Tableau.colors);
+        this.labels = this.shape
+            .reduce(
+                function(acc, size) {
+                    acc.labels.push(Array.range(size, acc.last));
+                    acc.last += size;
+                    return acc;
+                }, 
+                { labels: [], last: 1 }
+            )
+            .labels;
+        this.labelStrings = this.labels
+            .map(row => Array.sum(row.map(x => x + ""), ""));
+    }
+
+    draw() {
+        Renderer.push(this);
+        Renderer.translate(...Tableau.gridPosition);
+        Renderer.translate(...this.position);
+        Renderer.newRenderable(Layers.Tableau, _stubs => {
+            fill(color(this.color));
+            beginShape();
+            vertex(0, 0);
+            for (let i = 0; i < this.shape.length; i++) {
+                const len = this.shape[i];
+                vertex(len * Tableau.gridUnit, i       * Tableau.gridUnit);
+                vertex(len * Tableau.gridUnit, (i + 1) * Tableau.gridUnit);
+            }
+            vertex(0, this.shape.length * Tableau.gridUnit);
+            endShape(CLOSE);
+
+            fill("#102542");
+            textSize(Tableau.textSize);
+            for (let i = 0; i < this.labels.length; i++) {
+                text(this.labelStrings[i], 2, i * Tableau.gridUnit + Renderer.textHeight(Tableau.textSize) * 0.8);
+            }
+        });
+        Renderer.pop(this);
+        console.log(this.labels);
+    }
+}
